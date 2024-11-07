464f593f35907db3019eafa0708b9d2cba98dd72
Michael Melesse <micmelesse@gmail.com>
2024-11-08 05:02:21 +0530

    just backward

--- reproducer.py
+++ reproducer.py
@@ -24,28 +24,6 @@ sequence_parallel = True # Sequence Parallel has issues when size is large
 q, k, v = [torch.randn(batch_size, nheads, seqlen, headdim, device=device, dtype=dtype,
                     requires_grad=True) for _ in range(3)]
 
-def benchmark_forward(
-    fn, *inputs, repeats=10, desc="", verbose=True, amp=False, amp_dtype=torch.float16, **kwinputs
-):
-    """Use Pytorch Benchmark on the forward pass of an arbitrary function."""
-    if verbose:
-        print(desc, "- Forward pass")
-
-    def amp_wrapper(*inputs, **kwinputs):
-        with torch.autocast(device_type="cuda", dtype=amp_dtype, enabled=amp):
-            fn(*inputs, **kwinputs)
-
-    t = benchmark.Timer(
-        stmt="fn_amp(*inputs, **kwinputs)",
-        globals={"fn_amp": amp_wrapper, "inputs": inputs, "kwinputs": kwinputs},
-        num_threads=torch.get_num_threads(),
-    )
-    m = t.timeit(repeats)
-    if verbose:
-        print(m)
-    return t, m
-
-
 def benchmark_backward(
     fn,
     *inputs,
@@ -60,6 +38,7 @@ def benchmark_backward(
     """Use Pytorch Benchmark on the backward pass of an arbitrary function."""
     if verbose:
         print(desc, "- Backward pass")
+    
     with torch.autocast(device_type="cuda", dtype=amp_dtype, enabled=amp):
         y = fn(*inputs, **kwinputs)
         if type(y) is tuple:
@@ -87,51 +66,13 @@ def benchmark_backward(
         print(m)
     return t, m
 
-
-def benchmark_fwd_bwd(
-    fn,
-    *inputs,
-    grad=None,
-    repeats=10,
-    desc="",
-    verbose=True,
-    amp=False,
-    amp_dtype=torch.float16,
-    **kwinputs,
-):
-    """Use Pytorch Benchmark on the forward+backward pass of an arbitrary function."""
-    return (
-        benchmark_forward(
-            fn,
-            *inputs,
-            repeats=repeats,
-            desc=desc,
-            verbose=verbose,
-            amp=amp,
-            amp_dtype=amp_dtype,
-            **kwinputs,
-        ),
-        benchmark_backward(
-            fn,
-            *inputs,
-            grad=grad,
-            repeats=repeats,
-            desc=desc,
-            verbose=verbose,
-            amp=amp,
-            amp_dtype=amp_dtype,
-            **kwinputs,
-        ),
-    )
-
 try:
-    _, b0 = benchmark_fwd_bwd(
+    b = benchmark_backward(
         attention_triton, q, k, v, causal, headdim**(-0.5),
         sequence_parallel, repeats=repeats, verbose=True
     )
 except Exception as e:
     print(f"An error occurred: {e}")
-    b0 = float('inf')
 
-
+print(f"b: {b}")
 print("Success!")
